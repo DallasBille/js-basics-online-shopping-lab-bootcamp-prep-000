4691212bc11c67085d0c6951a040d98b1ac4b2b9
Dallas <dallasbille@gmail.com>
2018-10-24 01:41:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function addToCart(item) {
 function viewCart() {
   var list=[];
   for(let i=0; i<cart.length; i++){
-    list.push(`In your cart, you have ${cart[i]} at $${cart[i]}.`)
+    list.push(`In your cart, you have ${cart[i].itemPrice} at $${cart[i].itemPrice}.`)
 
 return list.join()
 }
